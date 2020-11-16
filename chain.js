5f3af53271b7bf0d570c1aef055126c0dccf74e3
Kaiden1221 <73253837+Kaiden1221@users.noreply.github.com>
2020-11-16 16:03:26 -0500

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,18 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            length: 100,
+            stiffness:0.5
+        }
+        this.chain=Constraint.create(options)
+        World.add(world, this.chain)
+    }
+    display(){
+        var pointA=this.chain.bodyA
+        var pointB=this.chain.bodyB
+        strokeWeight(5)
+        line(pointA.x, pointA.y, pointB.x, pointB.y)
+    }
+}
